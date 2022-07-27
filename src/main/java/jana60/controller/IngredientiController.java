0e7ba6bdc7b2f6ab5397c82a5979c81f9677d059
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-07-27 16:44:28 +0200

    Aggiunto inserimento ingredienti

--- src/main/java/jana60/controller/IngredientiController.java
+++ src/main/java/jana60/controller/IngredientiController.java
@@ -2,10 +2,15 @@ package jana60.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jana60.model.Ingredienti;
@@ -23,5 +28,27 @@ public class IngredientiController {
 		List<Ingredienti> IngredientiList = (List<Ingredienti>) repo.findAll();
 		model.addAttribute("IngredientiList", IngredientiList);
 		return "/ingredienti/list";
+	}
+
+	@GetMapping("/edit")
+	public String pizzaForm(Model model) {
+		model.addAttribute("listaIngredienti", repo.findAllByOrderByNome());
+		model.addAttribute("newIngrediente", new Ingredienti());
+		return "/ingredienti/edit";
+	}
+
+	@PostMapping("/edit")
+	public String saveIngrediente(@Valid @ModelAttribute("newIngrediente") Ingredienti formIngrediente,
+			BindingResult br, Model model) {
+		if (br.hasErrors()) {
+			// ricarico la pagina
+			model.addAttribute("ingredienti", repo.findAllByOrderByNome());
+			return "/ingredienti/edit";
+
+		} else {
+			// salvo la category
+			repo.save(formIngrediente);
+			return "redirect:/ingredienti/list";
+		}
 	}
 }
